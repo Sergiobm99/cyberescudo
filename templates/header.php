72fabb873eab22c1900eed852bcd1f5fbbc4aa24
Sergiobm99 <72201394+Sergiobm99@users.noreply.github.com>
2026-04-29 22:39:55 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -50,6 +50,7 @@
           <li><a href="<?= BASE_URL ?>/tool-revshell.php">🐚 Reverse Shell Generator</a></li>
           <li><a href="<?= BASE_URL ?>/tool-cron.php">⏱ Cron Parser</a></li>
           <li><a href="<?= BASE_URL ?>/tool-headers.php">📋 HTTP Header Analyzer</a></li>
+          <li><a href="<?= BASE_URL ?>/tool-wordlist.php">📝 Wordlist Generator</a></li>
         </ul>
       </li>
 
@@ -100,7 +101,7 @@
                 <li><a href="<?= BASE_URL ?>/tool-revshell.php">🐚 Reverse Shell Generator</a></li>
                 <li><a href="<?= BASE_URL ?>/tool-cron.php">⏱ Cron Parser</a></li>
                 <li><a href="<?= BASE_URL ?>/tool-headers.php">📋 HTTP Header Analyzer</a></li>
-                <li><a href="<?= BASE_URL ?>/tool-wordlist.php">📋 📝 Wordlist Generator</a></li>
+                <li><a href="<?= BASE_URL ?>/tool-wordlist.php">📝 Wordlist Generator</a></li>
             </ul>
         </li>
 
